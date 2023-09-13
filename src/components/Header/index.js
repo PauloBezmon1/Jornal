d2c7435463fc422906cef5937ce140045d9a4585
PauloBezmon1 <137209259+PauloBezmon1@users.noreply.github.com>
2023-09-13 10:47:59 -0300

    Add files via upload

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,29 @@
+import { Link } from "react-router-dom";
 import './header.css';
 
 function Header(){
      return(
-        
-         <div className="header">
-             <img className='icon' src='icon.png' alt='logo'/>
-             <div className='pesq'>
-           
-                </div>
-         </div>
+        <div>
+            <div className="header">
+                    <div>
+                        <img className='icon' src='icon.png' alt='logo'/>
+                    </div>
+                    <div className='pesq'>
 
+                    </div>
+
+            </div>
+                    <div className="catalogo">
+                        <Link className="link-home"   to="/">Negócios</Link>
+                        <Link className="link-home"   to="/">Política</Link>
+                        <Link className="link-home"   to="/">Esporte</Link>
+                        <Link className="link-home"   to="/">Tecnologia</Link>
+                        <Link className="link-home"   to="/">Comida</Link>
+                        <Link className="link-home"   to="/">Entretenimento</Link>
+                        <Link className="link-home"   to="/">Ciência</Link>
+                        <Link className="link-home"   to="/">Ambiente</Link>
+                    </div>
+         </div>   
      );
  }
 
